247136ecb212271ca28231f7c7f215337604de9e
Anton <lukyanoff1337@gmail.com>
2022-04-04 22:36:56 +0300

    added access filter for admin panel

--- src/main/java/by/lukanov/final_task/controller/filter/AccessFilter.java
+++ src/main/java/by/lukanov/final_task/controller/filter/AccessFilter.java
@@ -1,15 +1,19 @@
 package by.lukanov.final_task.controller.filter;
 
+import by.lukanov.final_task.command.PagePath;
+import by.lukanov.final_task.command.ParameterAndAttribute;
+import by.lukanov.final_task.entity.User;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "AccessFilter", urlPatterns = {"/pages/signin.jsp"},
+@WebFilter(filterName = "AccessFilter", urlPatterns = {"/pages/admin/*"},
 dispatcherTypes = {DispatcherType.FORWARD})
 public class AccessFilter implements Filter {
     private static final Logger logger = LogManager.getLogger();
@@ -20,9 +24,21 @@ public class AccessFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         HttpSession session = httpServletRequest.getSession(false);
 
-        chain.doFilter(request, response);
+        boolean loggedIn = session != null && session.getAttribute(ParameterAndAttribute.USER.getAttr()) != null;
+
+        if(loggedIn){
+            User user = (User) session.getAttribute(ParameterAndAttribute.USER.getAttr());
+            if(user.getRole() == User.Role.ADMIN || user.getRole() == User.Role.MANAGER){
+                chain.doFilter(request, response);
+            } else {
+                httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + PagePath.MAIN_PAGE);
+            }
+        } else {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + PagePath.MAIN_PAGE);
+        }
     }
 
     public void destroy() {
