f2f9bffe0b4c4a09092e561d932bc1958357caf7
Anton <lukyanoff1337@gmail.com>
2022-04-05 10:18:33 +0300

    main deleted

--- src/main/java/by/lukanov/final_task/Main.java
+++
@@ -1,9 +0,0 @@
-package by.lukanov.final_task;
-
-import by.lukanov.final_task.exception.DaoException;
-import by.lukanov.final_task.util.PasswordEncoder;
-
-public class Main {
-    public static void main(String[] args) throws DaoException {
-    }
-}
