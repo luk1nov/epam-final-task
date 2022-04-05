1077956937f263a6e495892d6f2275a40dd67b6b
Anton <lukyanoff1337@gmail.com>
2022-04-05 14:33:08 +0300

    fixed crud method

--- src/main/java/by/lukanov/final_task/model/dao/BaseDao.java
+++ src/main/java/by/lukanov/final_task/model/dao/BaseDao.java
@@ -1,4 +1,4 @@
-package by.lukanov.final_task.dao;
+package by.lukanov.final_task.model.dao;
 
 import by.lukanov.final_task.entity.AbstractEntity;
 import by.lukanov.final_task.exception.DaoException;
@@ -8,6 +8,6 @@ import java.util.List;
 public interface BaseDao<T extends AbstractEntity>{
     boolean insert(T t) throws DaoException;
     boolean delete(T t) throws DaoException;
-    List<T> findAllUsers() throws DaoException;
+    List<T> findAll() throws DaoException;
     boolean update(T t) throws DaoException;
 }
