7b3b9d38f7691a8aa26f0131158eab12b031be3c
Anton <lukyanoff1337@gmail.com>
2022-04-05 14:42:13 +0300

    changed to class

--- src/main/java/by/lukanov/final_task/command/ParameterAndAttribute.java
+++ src/main/java/by/lukanov/final_task/command/ParameterAndAttribute.java
@@ -1,25 +1,23 @@
 package by.lukanov.final_task.command;
 
-public enum ParameterAndAttribute {
-    COMMAND("command"),
-    REGISTRATION_PASSWORD("register_password"),
-    REGISTRATION_REPEATED_PASSWORD("register_repeated_password"),
-    REGISTRATION_EMAIL("register_email"),
-    REGISTRATION_NAME("register_name"),
-    REGISTRATION_SURNAME("register_surname"),
-    USER_ID("user_id"),
-    USER_NAME("userName"),
-    USER_SURNAME("userSurname"),
-    USER_EMAIL("userEmail"),
-    USER_PASS("userPass"),
-    USER_ROLE("userRole"),
-    USER_STATUS("userStatus"),
-    MESSAGE("message"),
-    USER("user"),
-    LOGIN_EMAIL("login_email"),
-    LOGIN_PASS("login_password"),
-    ALL_USERS("all_users"),
-    ADMIN_EDIT_USER("admin_edit_user");
+public class ParameterAndAttribute {
+//    request
+    public static final String COMMAND = "command";
+    public static final String USER_ID = "userId";
+    public static final String USER_NAME = "userName";
+    public static final String USER_SURNAME = "userSurname";
+    public static final String USER_EMAIL = "userEmail";
+    public static final String USER_PASS = "userPass";
+    public static final String USER_REPEAT_PASS = "userRepeatPass";
+    public static final String USER_ROLE = "userRole";
+    public static final String USER_STATUS = "userStatus";
+    public static final String MESSAGE = "message";
+    public static final String USER = "user";
+    public static final String ALL_USERS = "all_users";
+    public static final String ALL_CARS = "all_cars";
+
+//    session
+    public static final String LOGGED_USER = "loggedUser";
 
     private final String attr;
 
