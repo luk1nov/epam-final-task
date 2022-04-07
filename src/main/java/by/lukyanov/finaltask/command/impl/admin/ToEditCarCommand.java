188f13495a807dc31f546447a5cb03cf535bbd57
Anton <lukyanoff1337@gmail.com>
2022-04-08 02:23:48 +0300

    command added

---
+++ src/main/java/by/lukyanov/finaltask/command/impl/admin/ToEditCarCommand.java
@@ -0,0 +1,46 @@
+package by.lukyanov.finaltask.command.impl.admin;
+
+import by.lukyanov.finaltask.command.Command;
+import by.lukyanov.finaltask.command.Message;
+import by.lukyanov.finaltask.command.PagePath;
+import by.lukyanov.finaltask.command.Router;
+import by.lukyanov.finaltask.entity.Car;
+import by.lukyanov.finaltask.exception.CommandException;
+import by.lukyanov.finaltask.exception.ServiceException;
+import by.lukyanov.finaltask.model.service.impl.CarServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+
+import static by.lukyanov.finaltask.command.ParameterAndAttribute.*;
+
+public class ToEditCarCommand implements Command {
+    private static final Logger logger = LogManager.getLogger();
+    private static final CarServiceImpl carService = new CarServiceImpl();
+
+    @Override
+    public Router execute(HttpServletRequest request) throws CommandException {
+        Router router = new Router();
+        String carId = request.getParameter(CAR_ID).strip();
+        logger.debug("request id " + carId);
+        try {
+            Optional<Car> optionalCar = carService.findCarById(carId);
+            if (optionalCar.isPresent()){
+                Car car = optionalCar.get();
+                logger.debug("car found " + car.getId());
+                router.setPagePath(PagePath.ADMIN_EDIT_CAR);
+                request.setAttribute(CAR, car);
+            } else{
+                logger.debug("car not found");
+                router.setPagePath(PagePath.FAIL_PAGE);
+                request.setAttribute(MESSAGE, Message.CAN_NOT_EDIT_USER);
+            }
+        } catch (ServiceException e) {
+            logger.error("Command exception trying find car by id");
+            throw new CommandException(e);
+        }
+        return router;
+    }
+}
