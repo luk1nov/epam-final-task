3f1bb5afee79cbe76e64c84dfb648c358b487723
Anton Lukyanov <antoshal@gmail.com>
2022-05-30 20:17:01 +0300

    add new methods in OrderService

--- src/main/java/by/lukyanov/finaltask/model/service/OrderService.java
+++ src/main/java/by/lukyanov/finaltask/model/service/OrderService.java
@@ -1,28 +1,39 @@
 package by.lukyanov.finaltask.model.service;
 
-import by.lukyanov.finaltask.entity.Car;
-import by.lukyanov.finaltask.entity.Order;
-import by.lukyanov.finaltask.entity.OrderStatus;
-import by.lukyanov.finaltask.entity.User;
+import by.lukyanov.finaltask.entity.*;
 import by.lukyanov.finaltask.exception.ServiceException;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface OrderService {
+    List<Order> findAllOrders(String page) throws ServiceException;
+
     List<Order> findAllOrdersByUserId(long userId) throws ServiceException;
 
     List<Order> findActiveOrderDatesByCarId(long carId) throws ServiceException;
 
     boolean addOrder(Car car, User user, String orderDateRange) throws ServiceException;
 
-    List<Order> findOrdersByOrderStatus(OrderStatus status) throws ServiceException;
-
-    boolean cancelOrder(Order order) throws ServiceException;
+    List<Order> findOrdersByOrderStatus(OrderStatus status, String pageNumber) throws ServiceException;
 
     Optional<Order> findOrderById(String orderId) throws ServiceException;
 
     boolean updateOrderStatus(OrderStatus orderStatus, String orderId) throws ServiceException;
 
-    boolean declineOrder(String orderId, String message) throws ServiceException;
+    boolean cancelOrder(String orderId, String message) throws ServiceException;
+
+    boolean cancelOrder(String orderId) throws ServiceException;
+
+    boolean deleteOrder(String orderId) throws ServiceException;
+
+    boolean completeOrder(Map<String, String> reportData, InputStream reportPhoto) throws ServiceException;
+
+    Optional<OrderReport> findOrderReportById(String reportId) throws ServiceException;
+
+    int countAllOrders() throws ServiceException;
+
+    int countOrdersByStatus(OrderStatus status) throws ServiceException;
 }
