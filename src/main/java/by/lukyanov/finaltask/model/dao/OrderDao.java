4dcf2e8030366e4073f3852a44441ed158d7dacb
Anton Lukyanov <antoshal@gmail.com>
2022-05-30 20:17:14 +0300

    added pagination support

--- src/main/java/by/lukyanov/finaltask/model/dao/OrderDao.java
+++ src/main/java/by/lukyanov/finaltask/model/dao/OrderDao.java
@@ -1,9 +1,11 @@
 package by.lukyanov.finaltask.model.dao;
 
 import by.lukyanov.finaltask.entity.Order;
+import by.lukyanov.finaltask.entity.OrderReport;
 import by.lukyanov.finaltask.entity.OrderStatus;
 import by.lukyanov.finaltask.exception.DaoException;
 
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -15,13 +17,18 @@ public interface OrderDao extends BaseDao<Order>{
 
     boolean checkCarAvailabilityByDateRange(LocalDate beginDate, LocalDate endDate, long carId) throws DaoException; //todo
 
-    List<Order> findOrdersByOrderStatus(OrderStatus orderStatus) throws DaoException;
-
-    boolean cancelOrder(Order order) throws DaoException;
+    List<Order> findOrdersByOrderStatus(OrderStatus orderStatus, int limit, int offset) throws DaoException;
 
     Optional<Order> findById(long id) throws DaoException;
 
-    boolean updateOrderStatus(OrderStatus orderStatus, long id) throws DaoException;
-    
-    boolean declineOrder(Order order) throws DaoException;
+    boolean cancelOrder(Order order) throws DaoException;
+
+    boolean completeOrder(Order order, InputStream reportPhoto) throws DaoException;
+
+    Optional<OrderReport> findOrderReportById(long id) throws DaoException;
+
+    int countAllOrders() throws DaoException;
+
+    int countOrdersByStatus(OrderStatus status) throws DaoException;
 }
+
