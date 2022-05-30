7390d9b53747a3d8425bb99916d4332a4c3d710d
Anton Lukyanov <antoshal@gmail.com>
2022-05-30 20:17:53 +0300

    upd

--- src/main/java/by/lukyanov/finaltask/mapper/RowMapper.java
+++ src/main/java/by/lukyanov/finaltask/mapper/RowMapper.java
@@ -1,10 +1,12 @@
 package by.lukyanov.finaltask.mapper;
 
 import by.lukyanov.finaltask.entity.AbstractEntity;
+import by.lukyanov.finaltask.exception.DaoException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface RowMapper<T extends AbstractEntity> {
-    Optional<T> mapRow(ResultSet rs);
+    Optional<T> mapRow(ResultSet rs) throws SQLException, DaoException;
 }
