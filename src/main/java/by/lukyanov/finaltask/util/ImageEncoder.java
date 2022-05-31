6054650432c51c4251fe09a25ceffff71d68b49e
Anton <lukyanoff1337@gmail.com>
2022-05-31 13:50:36 +0300

    refactor

--- src/main/java/by/lukyanov/finaltask/util/ImageEncoder.java
+++ src/main/java/by/lukyanov/finaltask/util/ImageEncoder.java
@@ -1,19 +1,9 @@
 package by.lukyanov.finaltask.util;
 
-import by.lukyanov.finaltask.exception.DaoException;
-import by.lukyanov.finaltask.exception.ServiceException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.SQLException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public final class ImageEncoder {
-    private static final Logger logger = LogManager.getLogger();
     private static ImageEncoder instance;
 
     private ImageEncoder() {
@@ -26,40 +16,12 @@ public final class ImageEncoder {
         return instance;
     }
 
-    public String decodeBlob(Blob blob) throws DaoException {
-        String base64Image = null;
-        if (blob != null){
-            try (InputStream inputStream = blob.getBinaryStream();
-                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
-                byte[] buffer = new byte[4096];
-                int bytesRead = -1;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                byte[] imageBytes = outputStream.toByteArray();
-                base64Image = Base64.getEncoder().encodeToString(imageBytes);
-            } catch (SQLException | IOException e) {
-                logger.error("Dao exception trying encode blob", e);
-                throw new DaoException(e);
-            }
+    public String decodeImage(byte[] byteImage){
+        String image = null;
+        if(byteImage != null){
+            byte[] encodeImageBytes = Base64.getEncoder().encode(byteImage);
+            image = new String(encodeImageBytes, StandardCharsets.UTF_8);
         }
-        return base64Image;
-    }
-
-    public String decodeInputStream(InputStream is) throws ServiceException {
-        String base64Image;
-            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
-                byte[] buffer = new byte[4096];
-                int bytesRead = -1;
-                while ((bytesRead = is.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                byte[] imageBytes = outputStream.toByteArray();
-                base64Image = Base64.getEncoder().encodeToString(imageBytes);
-            } catch (IOException e) {
-                logger.error("Dao exception trying encode blob", e);
-                throw new ServiceException(e);
-            }
-        return base64Image;
+        return image;
     }
 }
