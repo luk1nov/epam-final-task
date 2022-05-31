34a8ed5fb9df471ec46d51043d6647c2fdd7b8f9
Anton <lukyanoff1337@gmail.com>
2022-05-31 13:50:04 +0300

    add mapper for Car

---
+++ src/main/java/by/lukyanov/finaltask/mapper/impl/CarRowMapper.java
@@ -0,0 +1,55 @@
+package by.lukyanov.finaltask.mapper.impl;
+
+import by.lukyanov.finaltask.entity.Car;
+import by.lukyanov.finaltask.entity.CarCategory;
+import by.lukyanov.finaltask.entity.CarInfo;
+import by.lukyanov.finaltask.exception.DaoException;
+import by.lukyanov.finaltask.mapper.RowMapper;
+import by.lukyanov.finaltask.util.ImageEncoder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class CarRowMapper implements RowMapper<Car> {
+    private static final Logger logger = LogManager.getLogger();
+    private static CarRowMapper instance;
+
+    private CarRowMapper() {
+    }
+
+    public static CarRowMapper getInstance(){
+        if (instance == null){
+            instance = new CarRowMapper();
+        }
+        return instance;
+    }
+
+    @Override
+    public Optional<Car> mapRow(ResultSet resultSet) throws DaoException {
+        ImageEncoder imageEncoder = ImageEncoder.getInstance();
+        Optional<Car> optionalCar;
+        try {
+            Car car = new Car.CarBuilder()
+                    .id(resultSet.getLong("car_id"))
+                    .brand(resultSet.getString("brand"))
+                    .model(resultSet.getString("model"))
+                    .vin(resultSet.getString("vin_code"))
+                    .regularPrice(resultSet.getBigDecimal("regular_price"))
+                    .salePrice(resultSet.getString("sale_price") != null ? resultSet.getBigDecimal("sale_price") : null)
+                    .active(resultSet.getBoolean("is_active"))
+                    .image(imageEncoder.decodeImage(resultSet.getBytes("image")))
+                    .carInfo(new CarInfo(resultSet.getDouble("acceleration"), resultSet.getInt("power"),
+                            CarInfo.Drivetrain.valueOf(resultSet.getString("drivetrain").toUpperCase())))
+                    .category(new CarCategory(resultSet.getString("car_category_title")))
+                    .build();
+            optionalCar = Optional.of(car);
+        } catch (SQLException e){
+            logger.error("Can not read resultset", e);
+            optionalCar = Optional.empty();
+        }
+        return optionalCar;
+    }
+}
