0f1ada60bad2ea3937ef11e14c0de70733ed50ff
Anton Lukyanov <antoshal@gmail.com>
2022-06-16 13:49:16 +0300

    added new pages

--- src/main/java/by/lukyanov/finaltask/command/PagePath.java
+++ src/main/java/by/lukyanov/finaltask/command/PagePath.java
@@ -33,6 +33,7 @@ public final class PagePath {
     public static final String USER_ACCOUNT = "pages/user-account.jsp";
     public static final String USER_ACCOUNT_ORDERS = "pages/user-account-orders.jsp";
     public static final String CAR_PAGE = "pages/car-page.jsp";
+    public static final String CATEGORIES_PAGE = "pages/shop-categories.jsp";
     public static final String SUCCESSFUL_ORDER = "pages/order/successful-order.jsp";
     public static final String FAILED_ORDER = "pages/order/failed-order.jsp";
     public static final String ORDER_REPORT = "pages/order/return-order-form.jsp";
@@ -40,12 +41,29 @@ public final class PagePath {
     public static final String TO_LOG_OUT = "controller?command=log_out";
     public static final String TO_CAR_PAGE = "controller?command=to_car_page";
     public static final String TO_USER_ORDERS = "controller?command=find_all_user_orders";
-    public static final String TO_CAR_CAR_CATEGORY_PAGE = "controller?command=to_car_category_page";
-    public static final String TO_UNVERIFIED_USERS = "controller?command=admin_to_unverified_users";
+    public static final String TO_CAR_CATEGORY_PAGE = "controller?command=to_car_category_page";
+    public static final String TO_ALL_CARS_CATEGORY_PAGE = "controller?command=to_all_cars";
+    public static final String TO_ADMIN_UNVERIFIED_USERS = "controller?command=admin_to_unverified_users";
     public static final String TO_ADMIN_PROCESSING_ORDERS = "controller?command=admin_find_processing_orders";
     public static final String TO_ADMIN_ALL_ORDERS = "controller?command=admin_find_all_orders";
     public static final String TO_ADMIN_ALL_CARS = "controller?command=admin_to_all_cars";
     public static final String TO_ADMIN_ALL_USERS = "controller?command=admin_to_all_users";
+    public static final String TO_ADMIN_COMPLETED_ORDERS = "controller?command=admin_find_completed_orders";
+    public static final String TO_ADMIN_EDIT_CAR = "controller?command=admin_to_edit_car";
+    public static final String TO_ADMIN_SEARCH_CAR = "controller?command=admin_search_car";
+    public static final String TO_ADMIN_SEARCH_USER = "controller?command=admin_search_user";
+    public static final String TO_ADMIN_SEARCH_ORDER = "controller?command=admin_search_order";
+    public static final String TO_ADMIN_REPAIRING_CARS = "controller?command=admin_to_repairing_cars";
+    public static final String TO_ADMIN_SHOW_ORDER_REPORT = "controller?command=admin_show_order_report";
+    public static final String TO_ADMIN_EDIT_USER = "controller?command=admin_to_edit_user";
+    public static final String TO_ADMIN_DECLINE_ORDER = "controller?command=admin_to_decline_order";
+    public static final String TO_FIND_ALL_USER_ORDERS = "controller?command=find_all_user_orders";
+    public static final String TO_GO_USER_ACCOUNT = "controller?command=to_user_account";
+    public static final String TO_GO_RETURN_CAR = "controller?command=to_return_car";
+    public static final String TO_ADMIN_ALL_CATEGORIES = "controller?command=admin_to_all_car_categories";
+    public static final String TO_ADMIN_EDIT_CATEGORY = "controller?command=admin_to_edit_car_category";
+    public static final String TO_ADMIN_ADD_NEW_CAR = "controller?command=admin_to_add_new_car";
+    public static final String TO_SIGN_IN = "controller?command=to_sign_in_page";
 
     private PagePath() {
     }
