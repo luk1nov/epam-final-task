3173f0167fcc446f9af194b3ce695ba781424cc4
Anton Lukyanov <antoshal@gmail.com>
2022-06-16 13:36:50 +0300

    added search by title

--- src/main/java/by/lukyanov/finaltask/model/dao/CarCategoryDao.java
+++ src/main/java/by/lukyanov/finaltask/model/dao/CarCategoryDao.java
@@ -6,5 +6,8 @@ import by.lukyanov.finaltask.exception.DaoException;
 import java.util.Optional;
 
 public interface CarCategoryDao extends BaseDao<CarCategory> {
-    Optional<CarCategory> findById(String id) throws DaoException;
+    Optional<CarCategory> findById(long id) throws DaoException;
+
+    Optional<CarCategory> findCarCategoryByTitle(String title) throws DaoException;
+
 }
