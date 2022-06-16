2503497bffe1b2f5026aeb4ab6a9a0546ab3e73b
Anton Lukyanov <antoshal@gmail.com>
2022-06-16 13:43:27 +0300

    added default method to generate url

--- src/main/java/by/lukyanov/finaltask/command/Command.java
+++ src/main/java/by/lukyanov/finaltask/command/Command.java
@@ -5,4 +5,14 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public interface Command {
     Router execute(HttpServletRequest request) throws CommandException;
+
+    default String generateUrlWithAttr(String url, String attributeName, String attributeValue){
+        StringBuilder stringBuilder = new StringBuilder()
+                .append(url);
+        if(attributeName != null && attributeValue != null && !attributeName.isBlank() && !attributeValue.isBlank()){
+            stringBuilder.append(attributeName)
+                    .append(attributeValue);
+        }
+        return stringBuilder.toString();
+    }
 }
