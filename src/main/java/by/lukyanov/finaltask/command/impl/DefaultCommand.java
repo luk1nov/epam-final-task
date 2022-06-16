43b954e0a20ebb3ecf4ca034cf62dc99e435cb9b
Anton Lukyanov <antoshal@gmail.com>
2022-06-16 13:49:01 +0300

    upd

--- src/main/java/by/lukyanov/finaltask/command/impl/DefaultCommand.java
+++ src/main/java/by/lukyanov/finaltask/command/impl/DefaultCommand.java
@@ -1,15 +1,20 @@
 package by.lukyanov.finaltask.command.impl;
 
 import by.lukyanov.finaltask.command.Command;
-import by.lukyanov.finaltask.command.PagePath;
 import by.lukyanov.finaltask.command.Router;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import static by.lukyanov.finaltask.command.PagePath.MAIN_PAGE;
+import static by.lukyanov.finaltask.command.ParameterAttributeName.CURRENT_PAGE;
 
 public class DefaultCommand implements Command {
     @Override
     public Router execute(HttpServletRequest request) {
         Router router = new Router();
-        router.setPagePath(PagePath.MAIN_PAGE);
+        HttpSession session = request.getSession();
+        session.setAttribute(CURRENT_PAGE, MAIN_PAGE);
+        router.setPagePath(MAIN_PAGE);
         return router;
     }
 }
