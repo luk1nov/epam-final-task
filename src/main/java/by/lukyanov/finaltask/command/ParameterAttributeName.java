89d4ab422a93af4ed7ebe0d7120013a9165d6483
Anton Lukyanov <antoshal@gmail.com>
2022-06-22 02:20:29 +0300

    upd

--- src/main/java/by/lukyanov/finaltask/command/ParameterAttributeName.java
+++ src/main/java/by/lukyanov/finaltask/command/ParameterAttributeName.java
@@ -3,7 +3,6 @@ package by.lukyanov.finaltask.command;
 public class ParameterAttributeName {
 //    request
     public static final String COMMAND = "command";
-    public static final String LANGUAGE = "language";
     public static final String USER_ID = "userId";
     public static final String USER_NAME = "userName";
     public static final String USER_SURNAME = "userSurname";
@@ -16,8 +15,6 @@ public class ParameterAttributeName {
     public static final String USER_STATUS = "userStatus";
     public static final String MESSAGE = "message";
     public static final String USER = "user";
-    public static final String ALL_USERS = "all_users";
-    public static final String ALL_CARS = "all_cars";
     public static final String CAR = "car";
     public static final String CAR_ID = "carId";
     public static final String CAR_BRAND = "carBrand";
