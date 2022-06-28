3e6e402086a70c86deab41d8135de67c571b321b
Anton <lukyanoff1337@gmail.com>
2022-06-28 19:11:46 +0300

    order factory added

---
+++ src/test/java/by/lukyanov/finaltask/factory/OrderFactory.java
@@ -0,0 +1,32 @@
+package by.lukyanov.finaltask.factory;
+
+import by.lukyanov.finaltask.entity.Order;
+import by.lukyanov.finaltask.entity.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static by.lukyanov.finaltask.factory.CarFactory.createCar;
+import static by.lukyanov.finaltask.factory.UserFactory.createUser;
+
+public class OrderFactory {
+    public static final long id = 1L;
+    public static final LocalDate beginDate = LocalDate.now();
+    public static final LocalDate endDate = LocalDate.now();
+    public static final OrderStatus status = OrderStatus.ACTIVE;
+    public static final String message = "message";
+    public static final BigDecimal price = new BigDecimal(1000);
+
+    public static Order createOrder(){
+        return new Order.OrderBuilder()
+                .id(id)
+                .beginDate(beginDate)
+                .endDate(endDate)
+                .orderStatus(status)
+                .message(message)
+                .user(createUser())
+                .car(createCar())
+                .price(price)
+                .build();
+    }
+}
