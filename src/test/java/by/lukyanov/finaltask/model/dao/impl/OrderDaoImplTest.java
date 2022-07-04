2ccfe858165bdf7735ee96a25d1f9030a86f8472
Anton <lukyanoff1337@gmail.com>
2022-07-05 01:49:23 +0300

    test created

---
+++ src/test/java/by/lukyanov/finaltask/model/dao/impl/OrderDaoImplTest.java
@@ -0,0 +1,72 @@
+package by.lukyanov.finaltask.model.dao.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDaoImplTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findOrdersByUserId() {
+    }
+
+    @Test
+    void findActiveOrderDatesByCarId() {
+    }
+
+    @Test
+    void checkCarAvailabilityByDateRange() {
+    }
+
+    @Test
+    void findOrdersByOrderStatus() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void completeOrder() {
+    }
+
+    @Test
+    void findOrderReportById() {
+    }
+
+    @Test
+    void countAllOrders() {
+    }
+
+    @Test
+    void countOrdersByStatus() {
+    }
+
+    @Test
+    void countOrdersByUserId() {
+    }
+
+    @Test
+    void searchOrders() {
+    }
+}
---
+++ src/test/java/by/lukyanov/finaltask/model/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,72 @@
+package by.lukyanov.finaltask.model.dao.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoImplTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void findUserByPhone() {
+    }
+
+    @Test
+    void updateBalance() {
+    }
+
+    @Test
+    void updateUserInfo() {
+    }
+
+    @Test
+    void updateDriverLicense() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+
+    @Test
+    void findUsersByStatus() {
+    }
+
+    @Test
+    void updateUserStatus() {
+    }
+
+    @Test
+    void countAllUsers() {
+    }
+
+    @Test
+    void countAllUsersByStatus() {
+    }
+
+    @Test
+    void searchUsers() {
+    }
+}
